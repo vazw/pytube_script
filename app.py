5faa2a20ea3b2b0e08052cb82b118f3b6ca7dd15
vazw <vaz@siamstr.com>
2024-07-24 18:02:33 +0700

    use pytubefix instead of pytube to fix HTTP 400 error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pathlib
-from pytube import Playlist, YouTube
+from pytubefix import Playlist, YouTube
 
 
 def youtube_download(url: str, mode: bool):
@@ -9,40 +9,47 @@ def youtube_download(url: str, mode: bool):
     for i, link in enumerate(playlist):
         yt = YouTube(link)
         if mode:
-            d_video = (
-                yt.streams.filter(progressive=True, file_extension="mp4")
-                .order_by("resolution")
-                .desc()
-                .first()
-            )
+            print(f"Downloading video: {yt.title}")
+            d_video = yt.streams.get_highest_resolution()
         else:
-            d_video = (
-                yt.streams.filter(only_audio=True, file_extension="mp4")
-                .order_by("abr")
-                .desc()
-                .first()
-            )
+            print(f"Downloading audio: {yt.title}")
+            d_video = yt.streams.get_audio_only()
         d_video.download("./Downloaded")
         print(i + 1, " Video is Downloaded.")
 
 
-def main(mode: int = 1):
-    print("ดาวน์โหลด YouTube Playlist ตัวอย่างลิงค์")
-    print("https://www.youtube.com/playlist?list={list-ID}")
+def vdo_download(url: str):
+    yt = YouTube(url)
+    (
+        yt.streams.filter(progressive=True, file_extension="mp4")
+        .first()
+        .download("./Downloaded")
+    )
+
+
+def main(mode: int):
     dmode = True if mode == 1 else False
-    while True:
-        try:
-            url = input("Please past youtube playlist url\n: ")
-            youtube_download(url, dmode)
-        except KeyboardInterrupt:
-            print("bye")
-            return
+    print(mode)
+    if int(mode) != 3:
+        while True:
+            print("ดาวน์โหลด YouTube Playlist ตัวอย่างลิงค์")
+            print("https://www.youtube.com/playlist?list={list-ID}")
+            try:
+                url = input("Please past youtube playlist url\n: ")
+                youtube_download(url, dmode)
+            except KeyboardInterrupt:
+                print("bye")
+                return
+    else:
+        print("ดาวน์โหลด YouTube Video")
+        url = input("Please past youtube url\n: ")
+        vdo_download(url)
 
 
 if __name__ == "__main__":
     if not os.path.exists("./Downloaded"):
         pathlib.Path("./Downloaded").mkdir(parents=True, exist_ok=True)
-    print("โปรดเลือกโหมดในการ Download\n1). Videos\n2). Audios")
+    print("โปรดเลือกโหมดในการ Download\n1). Playlist Videos\n2). Audios\n3). Video")
     mode = input("(defualt = 1) : ")
     if mode == "":
         mode = 1
