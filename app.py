08588c24a733e69a0be190e2381dd59a83411a59
vazw <vaz@siamstr.com>
2024-08-05 08:27:53 +0700

    add audio and playlist audio mode

--- app.py
+++ app.py
@@ -14,29 +14,34 @@ def youtube_download(url: str, mode: bool):
         else:
             print(f"Downloading audio: {yt.title}")
             d_video = yt.streams.get_audio_only()
-        d_video.download("./Downloaded")
-        print(i + 1, " Video is Downloaded.")
+        result = d_video.download("./Downloaded")
+        print(i + 1, f" Downloaded to {result}.")
 
 
 def vdo_download(url: str):
     yt = YouTube(url)
-    (
-        yt.streams.filter(progressive=True, file_extension="mp4")
-        .first()
-        .download("./Downloaded")
-    )
+    print(f"Downloading Video: {yt.title}")
+    result = yt.streams.get_highest_resolution().download("./Downloaded")
+    print(f"Downloaded Video: {result}")
+
+
+def audio_download(url: str):
+    yt = YouTube(url)
+    print(f"Downloading audio: {yt.title}")
+    result = yt.streams.get_audio_only().download("./Downloaded")
+    print(f"Downloaded Video: {result}")
 
 
 def main(mode: int):
     dmode = True if mode == 1 else False
-    print(mode)
     if int(mode) != 3:
         print("ดาวน์โหลด YouTube Playlist ตัวอย่างลิงค์")
         print("https://www.youtube.com/playlist?list={list-ID}")
         url = input("Please past youtube playlist url\n: ")
         youtube_download(url, dmode)
     else:
-        print("ดาวน์โหลด YouTube Video")
+        print("ดาวน์โหลด YouTube Video ตัวอย่างลิงค์")
+        print("https://www.youtube.com/watch?v=8UrwByNA2gk")
         url = input("Please past youtube url\n: ")
         vdo_download(url)
 
@@ -44,7 +49,9 @@ def main(mode: int):
 if __name__ == "__main__":
     if not os.path.exists("./Downloaded"):
         pathlib.Path("./Downloaded").mkdir(parents=True, exist_ok=True)
-    print("โปรดเลือกโหมดในการ Download\n1). Playlist Videos\n2). Audios\n3). Video")
+    print(
+        "โปรดเลือกโหมดในการ Download\n1). Playlist Videos\n2). Playlist Audios\n3). Video\n4). Audios"
+    )
     mode = input("(defualt = 1) : ")
     if mode == "":
         mode = 1
